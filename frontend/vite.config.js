2a2ed0f0cb19b5c1eef575016c501abfa9881e7d
lokesh0903sh <lokesh0903sh@gmail.com>
2024-10-24 18:57:27 +0530

    Project Completed Successfully

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
-  plugins: [react()],
   build: {
-    outDir: './src/dist', // This is the default; explicitly stating it is optional
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'), // Define '@' to point to 'src' directory
-    },
+    outDir: 'dist',  // Ensure the output folder is set to 'dist'
   },
 });
