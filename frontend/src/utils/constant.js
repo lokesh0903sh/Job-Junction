478d95b89957646968fc581e2aa0cf280a491e7b
lokesh0903sh <lokesh0903sh@gmail.com>
2024-10-25 02:17:39 +0530

    Project Completed Successfully

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const USER_API_END_POINT="http://localhost:8000/user";
-export const JOB_API_END_POINT="http://localhost:8000/job";
-export const APPLICATION_API_END_POINT="http://localhost:8000/application";
-export const COMPANY_API_END_POINT="http://localhost:8000/company";
+export const USER_API_END_POINT="https://job-junction-s6vn.onrender.com/user";
+export const JOB_API_END_POINT="https://job-junction-s6vn.onrender.com/job";
+export const APPLICATION_API_END_POINT="https://job-junction-s6vn.onrender.com/application";
+export const COMPANY_API_END_POINT="https://job-junction-s6vn.onrender.com/company";
