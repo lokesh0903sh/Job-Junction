6257e79fbf4293d49943cb29d8c236acb94a066e
lokesh0903sh <lokesh0903sh@gmail.com>
2024-10-24 23:31:38 +0530

    Project Completed Successfully

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:"job-junction-7zgh6ywm3-lokesh-sharmas-projects-a4159823.vercel.app",
+    origin:"https://job-junction-tau.vercel.app",
     credentials:true
 }
 
