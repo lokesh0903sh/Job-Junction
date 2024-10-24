f6da6e7c0f564e7888a78af48c98cca2925ebabe
lokesh0903sh <lokesh0903sh@gmail.com>
2024-10-25 03:09:46 +0530

    Project Completed Successfully

--- frontend/src/components/Job.jsx
+++ frontend/src/components/Job.jsx
@@ -8,6 +8,7 @@ import { useNavigate } from 'react-router-dom'
 const Job = ({job}) => {
     const navigate = useNavigate();
     // const jobId = "lsekdhjgdsnfvsdkjf";
+    const info = `${job?.description.substring(0, 200)}....`;
 
     const daysAgoFunction = (mongodbTime) => {
         const createdAt = new Date(mongodbTime);
@@ -37,8 +38,11 @@ const Job = ({job}) => {
 
             <div>
                 <h1 className='font-bold text-lg my-2'>{job?.title}</h1>
-                <p className='text-sm text-gray-600'>{job?.description}</p>
-            </div>
+                <p className='text-sm text-gray-600'>{info}
+                    <span className="text-[#F83002]" >
+                        ReadMore
+                    </span>
+                </p>            </div>
             <div className='flex items-center gap-2 mt-4'>
                 <Badge className={'text-blue-700 font-bold'} variant="ghost">{job?.position} Positions</Badge>
                 <Badge className={'text-[#F83002] font-bold'} variant="ghost">{job?.jobType}</Badge>
