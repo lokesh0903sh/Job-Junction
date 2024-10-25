f89380d3a006e044beba57ad7b5dcc40845f72a4
lokesh0903sh <lokesh0903sh@gmail.com>
2024-10-25 10:58:04 +0530

    Project Completed Successfully

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'https://job-junction-s6vn.onrender.com',
+    origin:'https://job-junction-livid.vercel.app',
     credentials:true
 }
 
--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const USER_API_END_POINT="https://job-junction-s6vn.onrender.com/user";
-export const JOB_API_END_POINT="https://job-junction-s6vn.onrender.com/job";
-export const APPLICATION_API_END_POINT="https://job-junction-s6vn.onrender.com/application";
-export const COMPANY_API_END_POINT="https://job-junction-s6vn.onrender.com/company";
+export const USER_API_END_POINT="https://job-junction-livid.vercel.app/user";
+export const JOB_API_END_POINT="https://job-junction-livid.vercel.app/job";
+export const APPLICATION_API_END_POINT="https://job-junction-livid.vercel.app/application";
+export const COMPANY_API_END_POINT="https://job-junction-livid.vercel.app/company";
