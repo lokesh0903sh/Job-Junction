22a482ec1e2ba22d290cf17f02a84ad4175c2a9a
lokesh0903sh <lokesh0903sh@gmail.com>
2024-11-06 20:48:01 +0530

    Project Completed Successfully

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -1,133 +1,165 @@
-import React, { useEffect, useState } from 'react'
-import Navbar from '../shared/Navbar'
-import { Label } from '../ui/label'
-import { Input } from '../ui/input'
-import { RadioGroup } from '../ui/radio-group'
-import { Button } from '../ui/button'
-import { Link, useNavigate } from 'react-router-dom'
-import axios from 'axios'
-import { USER_API_END_POINT } from '@/utils/constant'
-import { toast } from 'sonner'
-import { useDispatch, useSelector } from 'react-redux'
-import { setLoading, setUser } from '@/redux/authSlice'
-import { Loader2 } from 'lucide-react'
-import {motion} from "framer-motion";
+import React, { useEffect, useState } from "react";
+import Navbar from "../shared/Navbar";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import { RadioGroup } from "../ui/radio-group";
+import { Button } from "../ui/button";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { USER_API_END_POINT } from "@/utils/constant";
+import { toast } from "sonner";
+import { useDispatch, useSelector } from "react-redux";
+import { setLoading, setUser } from "@/redux/authSlice";
+import { Loader2 } from "lucide-react";
+import { motion } from "framer-motion";
+import { FaEye, FaEyeSlash } from "react-icons/fa"; // Assuming you're using react-icons
 
 const Login = () => {
+  const containerVariants = {
+    hidden: { opacity: 0, x: -100 },
+    visible: { opacity: 1, x: 0, transition: { duration: 0.5 } },
+    exit: { opacity: 0, x: 100, transition: { duration: 0.5 } },
+  };
 
-    const containerVariants = {
-        hidden: { opacity: 0, x: -100 },
-        visible: { opacity: 1, x: 0, transition: { duration: 0.5 } },
-        exit: { opacity: 0, x: 100, transition: { duration: 0.5 } },
-      };
-    const [input, setInput] = useState({
-        email: "",
-        password: "",
-        role: "",
-    });
-    const { loading,user } = useSelector(store => store.auth);
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
+  const [passwordVisible, setPasswordVisible] = useState(false);
 
-    const changeEventHandler = (e) => {
-        setInput({ ...input, [e.target.name]: e.target.value });
+  const togglePasswordVisibility = () => {
+    setPasswordVisible((prevVisible) => !prevVisible);
+  };
+
+  const [input, setInput] = useState({
+    email: "",
+    password: "",
+    role: "",
+  });
+  const { loading, user } = useSelector((store) => store.auth);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const changeEventHandler = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(setLoading(true));
+      const res = await axios.post(`${USER_API_END_POINT}/login`, input, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        withCredentials: true,
+      });
+      if (res.data.success) {
+        dispatch(setUser(res.data.user));
+        navigate("/");
+        toast.success(res.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.response.data.message);
+    } finally {
+      dispatch(setLoading(false));
     }
-
-    const submitHandler = async (e) => {
-        e.preventDefault();
-        try {
-            dispatch(setLoading(true));
-            const res = await axios.post(`${USER_API_END_POINT}/login`, input, {
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                withCredentials: true,
-            });
-            if (res.data.success) {
-                dispatch(setUser(res.data.user));
-                navigate("/");
-                toast.success(res.data.message);
-            }
-        } catch (error) {
-            console.log(error);
-            toast.error(error.response.data.message);
-        } finally {
-            dispatch(setLoading(false));
-        }
+  };
+  useEffect(() => {
+    if (user) {
+      navigate("/");
     }
-    useEffect(()=>{
-        if(user){
-            navigate("/");
-        }
-    },[])
-    return (
-        <div>
-            <Navbar />
-            <motion.div
-                key="login"
-                initial="hidden"
-                animate="visible"
-                exit="exit"
-                variants={containerVariants}>
-            <div className='flex items-center justify-center max-w-7xl mx-auto'>
-                <form onSubmit={submitHandler} className='w-1/2 border border-gray-200 rounded-lg p-4 my-10 shadow-xl'>
-                    <h1 className='font-bold text-xl mb-5'>Login</h1>
-                    <div className='my-2'>
-                        <Label>Email</Label>
-                        <Input
-                            type="email"
-                            value={input.email}
-                            name="email"
-                            onChange={changeEventHandler}
-                            placeholder="lokesh@gmail.com"
-                        />
-                    </div>
-
-                    <div className='my-2'>
-                        <Label>Password</Label>
-                        <Input
-                            type="password"
-                            value={input.password}
-                            name="password"
-                            onChange={changeEventHandler}
-                            placeholder="xxxxxxxxxxx"
-                        />
-                    </div>
-                    <div className='flex items-center justify-between'>
-                        <RadioGroup className="flex items-center gap-4 my-5">
-                            <div className="flex items-center space-x-2">
-                                <Input
-                                    type="radio"
-                                    name="role"
-                                    value="student"
-                                    checked={input.role === 'student'}
-                                    onChange={changeEventHandler}
-                                    className="cursor-pointer"
-                                />
-                                <Label htmlFor="r1">Student</Label>
-                            </div>
-                            <div className="flex items-center space-x-2">
-                                <Input
-                                    type="radio"
-                                    name="role"
-                                    value="recruiter"
-                                    checked={input.role === 'recruiter'}
-                                    onChange={changeEventHandler}
-                                    className="cursor-pointer"
-                                />
-                                <Label htmlFor="r2">Recruiter</Label>
-                            </div>
-                        </RadioGroup>
-                    </div>
-                    {
-                        loading ? <Button className="w-full my-4"> <Loader2 className='mr-2 h-4 w-4 animate-spin' /> Please wait </Button> : <Button type="submit" className="w-full my-4">Login</Button>
-                    }
-                    <span className='text-sm'>Don't have an account? <Link to="/signup" className='text-blue-600'>Signup</Link></span>
-                </form>
+  }, []);
+  return (
+    <div>
+      <Navbar />
+      <motion.div
+        key="login"
+        initial="hidden"
+        animate="visible"
+        exit="exit"
+        variants={containerVariants}
+      >
+        <div className="flex items-center justify-center max-w-7xl mx-auto">
+          <form
+            onSubmit={submitHandler}
+            className="w-1/2 border border-gray-200 rounded-lg p-4 my-10 shadow-xl"
+          >
+            <h1 className="font-bold text-xl mb-5">Login</h1>
+            <div className="my-2">
+              <Label>Email</Label>
+              <Input
+                type="email"
+                value={input.email}
+                name="email"
+                onChange={changeEventHandler}
+                placeholder="lokesh@gmail.com"
+              />
             </div>
-            </motion.div>
-        </div>
-    )
-}
 
-export default Login
+            <div className="my-2">
+              <Label>Password</Label>
+              <div className="flex items-center relative">
+                <Input
+                  type={passwordVisible ? "text" : "password"}
+                  value={input.password}
+                  name="password"
+                  onChange={changeEventHandler}
+                  placeholder="xxxxxxxxxxx"
+                />
+                <span
+                  onClick={togglePasswordVisibility}
+                  className="absolute right-4"
+                  id="eyeicon"
+                >
+                  {passwordVisible ? <FaEye /> : <FaEyeSlash />}
+                </span>
+              </div>
+            </div>
+            <div className="flex items-center justify-between">
+              <RadioGroup className="flex items-center gap-4 my-5">
+                <div className="flex items-center space-x-2">
+                  <Input
+                    type="radio"
+                    name="role"
+                    value="student"
+                    checked={input.role === "student"}
+                    onChange={changeEventHandler}
+                    className="cursor-pointer"
+                  />
+                  <Label htmlFor="r1">Student</Label>
+                </div>
+                <div className="flex items-center space-x-2">
+                  <Input
+                    type="radio"
+                    name="role"
+                    value="recruiter"
+                    checked={input.role === "recruiter"}
+                    onChange={changeEventHandler}
+                    className="cursor-pointer"
+                  />
+                  <Label htmlFor="r2">Recruiter</Label>
+                </div>
+              </RadioGroup>
+            </div>
+            {loading ? (
+              <Button className="w-full my-4">
+                {" "}
+                <Loader2 className="mr-2 h-4 w-4 animate-spin" /> Please wait{" "}
+              </Button>
+            ) : (
+              <Button type="submit" className="w-full my-4">
+                Login
+              </Button>
+            )}
+            <span className="text-sm">
+              Don't have an account?{" "}
+              <Link to="/signup" className="text-blue-600">
+                Signup
+              </Link>
+            </span>
+          </form>
+        </div>
+      </motion.div>
+    </div>
+  );
+};
+
+export default Login;
--- frontend/src/components/auth/Signup.jsx
+++ frontend/src/components/auth/Signup.jsx
@@ -1,190 +1,222 @@
-import React, { useEffect, useState } from 'react'
-import Navbar from '../shared/Navbar'
-import { Label } from '../ui/label'
-import { Input } from '../ui/input'
-import { RadioGroup } from '../ui/radio-group'
-import { Button } from '../ui/button'
-import { Link, useNavigate } from 'react-router-dom'
-import axios from 'axios'
-import { USER_API_END_POINT } from '@/utils/constant'
-import { toast } from 'sonner'
-import { useDispatch, useSelector } from 'react-redux'
-import { setLoading } from '@/redux/authSlice'
-import { Loader2 } from 'lucide-react'
-import { motion } from 'framer-motion';
+import React, { useEffect, useState } from "react";
+import Navbar from "../shared/Navbar";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import { RadioGroup } from "../ui/radio-group";
+import { Button } from "../ui/button";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { USER_API_END_POINT } from "@/utils/constant";
+import { toast } from "sonner";
+import { useDispatch, useSelector } from "react-redux";
+import { setLoading } from "@/redux/authSlice";
+import { Loader2 } from "lucide-react";
+import { motion } from "framer-motion";
+import { FaEye, FaEyeSlash } from "react-icons/fa"; // Assuming you're using react-icons
 
 const Signup = () => {
+  const [input, setInput] = useState({
+    fullname: "",
+    email: "",
+    phoneNumber: "",
+    password: "",
+    role: "",
+    file: "",
+  });
 
-    const [input, setInput] = useState({
-        fullname: "",
-        email: "",
-        phoneNumber: "",
-        password: "",
-        role: "",
-        file: ""
-    });
+  const [password, setPassword] = useState(""); // Separate state for password
+  const [passwordVisible, setPasswordVisible] = useState(false);
 
-    const containerVariants = {
-        hidden: { opacity: 0, x: -100 },
-        visible: { opacity: 1, x: 0, transition: { duration: 0.5 } },
-        exit: { opacity: 0, x: 100, transition: { duration: 0.5 } },
-      };
-    const {loading,user} = useSelector(store=>store.auth);
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
+  const togglePasswordVisibility = () => {
+    setPasswordVisible((prevVisible) => !prevVisible);
+  };
 
-    const changeEventHandler = (e) => {
-        setInput({ ...input, [e.target.name]: e.target.value });
+  const generatePassword = () => {
+    const length = 12;
+    const charset =
+      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
+    let generatedPassword = "";
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * charset.length);
+      generatedPassword += charset[randomIndex];
     }
-    const changeFileHandler = (e) => {
-        setInput({ ...input, file: e.target.files?.[0] });
-    }
-    const submitHandler = async (e) => {
-        e.preventDefault();
-        const formData = new FormData();    //formdata object
-        formData.append("fullname", input.fullname);
-        formData.append("email", input.email);
-        formData.append("phoneNumber", input.phoneNumber);
-        formData.append("password", input.password);
-        formData.append("role", input.role);
-        if (input.file) {
-            formData.append("file", input.file);
-        }
+    setPassword(generatedPassword); // Set the generated password
+  };
 
-        try {
-            dispatch(setLoading(true));
-            const res = await axios.post(`${USER_API_END_POINT}/register`, formData, {
-                headers: { 'Content-Type': "multipart/form-data" },
-                withCredentials: true,
-            });
-            if (res.data.success) {
-                navigate("/login");
-                toast.success(res.data.message);
-            }
-        } catch (error) {
-            console.log(error);
-            toast.error(error.response.data.message);
-        } finally{
-            dispatch(setLoading(false));
-        }
-    }
-    const generatePassword = () => {
-        const length = 10; // Set the desired password length
-        const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
-        let generatedPassword = "";
-        for (let i = 0; i < length; i++) {
-            const randomIndex = Math.floor(Math.random() * charset.length);
-            generatedPassword += charset[randomIndex];
-        }
-        setInput({ ...input, password: generatedPassword }); // Set generated password
-    };
+  const containerVariants = {
+    hidden: { opacity: 0, x: -100 },
+    visible: { opacity: 1, x: 0, transition: { duration: 0.5 } },
+    exit: { opacity: 0, x: 100, transition: { duration: 0.5 } },
+  };
+  const { loading, user } = useSelector((store) => store.auth);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    useEffect(()=>{
-        if(user){
-            navigate("/");
-        }
-    },[])
-    return (
-        <div>
-            <Navbar />
-            <motion.div
-                key="login"
-                initial="hidden"
-                animate="visible"
-                exit="exit"
-                variants={containerVariants}>
-            <div className='flex items-center justify-center max-w-7xl mx-auto'>
-                <form onSubmit={submitHandler} className='w-1/2 border border-gray-200 rounded-lg p-4 my-10 shadow-xl'>
-                    <h1 className='font-bold text-xl mb-5'>Sign Up</h1>
-                    <div className='my-2'>
-                        <Label>Full Name</Label>
-                        <Input
-                            type="text"
-                            value={input.fullname}
-                            name="fullname"
-                            onChange={changeEventHandler}
-                            placeholder="Lokesh Sharma"
-                        />
-                    </div>
-                    <div className='my-2'>
-                        <Label>Email</Label>
-                        <Input
-                            type="email"
-                            value={input.email}
-                            name="email"
-                            onChange={changeEventHandler}
-                            placeholder="lokesh@gmail.com"
-                        />
-                    </div>
-                    <div className='my-2'>
-                        <Label>Phone Number</Label>
-                        <Input
-                            type="text"
-                            value={input.phoneNumber}
-                            name="phoneNumber"
-                            onChange={changeEventHandler}
-                            placeholder="123456789"
-                        />
-                    </div>
-                    <div className='my-2 flex'>
-                        <div className="w-[50%]">
-                            <Label>Password</Label>
-                            <Input
-                                type="password"
-                                value={input.password}
-                                name="password"
-                                onChange={changeEventHandler}
-                                placeholder="xxxxxxxxxxx"
-                            />
-                        </div>
-                        <div className="mx-6">
-                            <Button className="my-6" onClick={generatePassword}>Generate Random Password</Button>
-                        </div>
-                    </div>
-                    <div className='flex items-center justify-between'>
-                        <RadioGroup className="flex items-center gap-4 my-5">
-                            <div className="flex items-center space-x-2">
-                                <Input
-                                    type="radio"
-                                    name="role"
-                                    value="student"
-                                    checked={input.role === 'student'}
-                                    onChange={changeEventHandler}
-                                    className="cursor-pointer"
-                                />
-                                <Label htmlFor="r1">Student</Label>
-                            </div>
-                            <div className="flex items-center space-x-2">
-                                <Input
-                                    type="radio"
-                                    name="role"
-                                    value="recruiter"
-                                    checked={input.role === 'recruiter'}
-                                    onChange={changeEventHandler}
-                                    className="cursor-pointer"
-                                />
-                                <Label htmlFor="r2">Recruiter</Label>
-                            </div>
-                        </RadioGroup>
-                        <div className='flex items-center gap-2'>
-                            <Label>Profile</Label>
-                            <Input
-                                accept="image/*"
-                                type="file"
-                                onChange={changeFileHandler}
-                                className="cursor-pointer"
-                            />
-                        </div>
-                    </div>
-                    {
-                        loading ? <Button className="w-full my-4"> <Loader2 className='mr-2 h-4 w-4 animate-spin' /> Please wait </Button> : <Button type="submit" className="w-full my-4">Signup</Button>
-                    }
-                    <span className='text-sm'>Already have an account? <Link to="/login" className='text-blue-600'>Login</Link></span>
-                </form>
+  const changeEventHandler = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
+  const changeFileHandler = (e) => {
+    setInput({ ...input, file: e.target.files?.[0] });
+  };
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(); //formdata object
+    formData.append("fullname", input.fullname);
+    formData.append("email", input.email);
+    formData.append("phoneNumber", input.phoneNumber);
+    formData.append("password", password);
+    formData.append("role", input.role);
+    if (input.file) {
+      formData.append("file", input.file);
+    }
+
+    try {
+      dispatch(setLoading(true));
+      const res = await axios.post(`${USER_API_END_POINT}/register`, formData, {
+        headers: { "Content-Type": "multipart/form-data" },
+        withCredentials: true,
+      });
+      if (res.data.success) {
+        navigate("/login");
+        toast.success(res.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.response.data.message);
+    } finally {
+      dispatch(setLoading(false));
+    }
+  };
+
+  useEffect(() => {
+    if (user) {
+      navigate("/");
+    }
+  }, []);
+  return (
+    <div>
+      <Navbar />
+      <motion.div
+        key="login"
+        initial="hidden"
+        animate="visible"
+        exit="exit"
+        variants={containerVariants}
+      >
+        <div className="flex items-center justify-center max-w-7xl mx-auto">
+          <form
+            onSubmit={submitHandler}
+            className="w-1/2 border border-gray-200 rounded-lg p-4 my-10 shadow-xl"
+          >
+            <h1 className="font-bold text-xl mb-5">Sign Up</h1>
+            <div className="my-2">
+              <Label>Full Name</Label>
+              <Input
+                type="text"
+                value={input.fullname}
+                name="fullname"
+                onChange={changeEventHandler}
+                placeholder="Lokesh Sharma"
+              />
             </div>
-            </motion.div>
+            <div className="my-2">
+              <Label>Email</Label>
+              <Input
+                type="email"
+                value={input.email}
+                name="email"
+                onChange={changeEventHandler}
+                placeholder="lokesh@gmail.com"
+              />
+            </div>
+            <div className="my-2">
+              <Label>Phone Number</Label>
+              <Input
+                type="text"
+                value={input.phoneNumber}
+                name="phoneNumber"
+                onChange={changeEventHandler}
+                placeholder="123456789"
+              />
+            </div>
+            <div className="my-2 flex gap-4">
+              <div className="w-full">
+                <Label>Password</Label>
+                <div className="flex items-center relative">
+                    <Input
+                    type={passwordVisible ? "text" : "password"}
+                    value={password}
+                    name="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="xxxxxxxxxxx"
+                    />
+                    <span onClick={togglePasswordVisibility} className='absolute right-3' id="eyeicon">
+                    {passwordVisible ? <FaEye /> : <FaEyeSlash />}
+                    </span>
+                </div>
+              </div>
+              <div>
+                <Button className="my-6" onClick={generatePassword}>
+                  Generate Random Password
+                </Button>
+              </div>
+            </div>
+            <div className="flex items-center justify-between">
+              <RadioGroup className="flex items-center gap-4 my-5">
+                <div className="flex items-center space-x-2">
+                  <Input
+                    type="radio"
+                    name="role"
+                    value="student"
+                    checked={input.role === "student"}
+                    onChange={changeEventHandler}
+                    className="cursor-pointer"
+                  />
+                  <Label htmlFor="r1">Student</Label>
+                </div>
+                <div className="flex items-center space-x-2">
+                  <Input
+                    type="radio"
+                    name="role"
+                    value="recruiter"
+                    checked={input.role === "recruiter"}
+                    onChange={changeEventHandler}
+                    className="cursor-pointer"
+                  />
+                  <Label htmlFor="r2">Recruiter</Label>
+                </div>
+              </RadioGroup>
+              <div className="flex items-center gap-2">
+                <Label>Profile</Label>
+                <Input
+                  accept="image/*"
+                  type="file"
+                  onChange={changeFileHandler}
+                  className="cursor-pointer"
+                />
+              </div>
+            </div>
+            {loading ? (
+              <Button className="w-full my-4">
+                {" "}
+                <Loader2 className="mr-2 h-4 w-4 animate-spin" /> Please wait{" "}
+              </Button>
+            ) : (
+              <Button type="submit" className="w-full my-4">
+                Signup
+              </Button>
+            )}
+            <span className="text-sm">
+              Already have an account?{" "}
+              <Link to="/login" className="text-blue-600">
+                Login
+              </Link>
+            </span>
+          </form>
         </div>
-    )
-}
+      </motion.div>
+    </div>
+  );
+};
 
-export default Signup
+export default Signup;
